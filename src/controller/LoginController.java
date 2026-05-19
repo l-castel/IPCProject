397ae76dfaaf3753ff97c39c752aa6f3852693a6
lcastel <lolacast@proton.me>
2026-05-19 15:39:48 +0200

    chore: generated login controller boilerplate

---
+++ src/controller/LoginController.java
@@ -0,0 +1,27 @@
+package controller;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private TextField usernameField;
+    @javafx.fxml.FXML
+    private Button btnLogin;
+    @javafx.fxml.FXML
+    private Label feedbackLabel;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+    @javafx.fxml.FXML
+    private Button btnRegister;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRegister(ActionEvent actionEvent) {
+    }
+}

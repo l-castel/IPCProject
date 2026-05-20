0cc9cc98dcfc871f91a1066c9c8d371eb49349dc
laurapatriciaantoneac-ctrl <laurapatriciaantoneac@gmail.com>
2026-05-20 17:53:20 +0200

    Added function buttons as modify and add windows

--- src/controller/ModifyMapController.java
+++ src/controller/ModifyMapController.java
@@ -4,15 +4,53 @@
  */
 package controller;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.ImageCursor;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.util.converter.LocalDateStringConverter;
+import upv.ipc.sportlib.SportActivityApp;
+import upv.ipc.sportlib.User;
+import upv.ipc.sportlib.MapRegion;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import java.time.LocalDate;
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.util.converter.LocalDateStringConverter;
+import java.time.temporal.ChronoUnit;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import mapademo.MapaDemoApp;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import java.util.Optional;
 /**
  * FXML Controller class
  *
@@ -21,23 +59,21 @@ import javafx.scene.image.ImageView;
 public class ModifyMapController implements Initializable {
 
     @FXML
-    private TextField mapNameField;
+    private TextField name;
     @FXML
-    private Button searchFileBtn;
+    private Button searchButton;
     @FXML
-    private ImageView mapImageView;
+    private TextField maxLatitud;
     @FXML
-    private TextField maxLatField;
+    private TextField minLatitud;
     @FXML
-    private TextField minLatField;
+    private TextField maxLongitud;
     @FXML
-    private TextField maxLonField;
+    private TextField minLongitud;
     @FXML
-    private TextField minLonField;
+    private Button gpxButton;
     @FXML
-    private Button uploadGpxBtn;
-    @FXML
-    private Button confirmAddBtn;
+    private Button modifyButton;
 
     /**
      * Initializes the controller class.

38e39a8cae22b178c8eec306accc6c8032339341
laurapatriciaantoneac-ctrl <laurapatriciaantoneac@gmail.com>
2026-05-20 16:41:35 +0200

    MapController updated. Complete initialize method

---
+++ src/controller/ModifyMapController.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author laura
+ */
+public class ModifyMapController implements Initializable {
+
+    @FXML
+    private TextField mapNameField;
+    @FXML
+    private Button searchFileBtn;
+    @FXML
+    private ImageView mapImageView;
+    @FXML
+    private TextField maxLatField;
+    @FXML
+    private TextField minLatField;
+    @FXML
+    private TextField maxLonField;
+    @FXML
+    private TextField minLonField;
+    @FXML
+    private Button uploadGpxBtn;
+    @FXML
+    private Button confirmAddBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleSearchFile(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleUploadGpx(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleConfirmAdd(ActionEvent event) {
+    }
+    
+}
